662ae8ef4d100abb1918f9754c9f8e3b4bb5009b
Pedro Peres <166012767+PePeres007@users.noreply.github.com>
2025-09-25 21:28:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
-def busca_binaria(lista, alvo, qtdeComparacoes=0):
+def busca_binaria(lista, alvo, quant_comp=0):
 	inicio = 0
 	fim = len(lista) - 1
-	qtdeComparacoes += 1
+	quant_comp += 1
 	if inicio <= fim:
 		meio = (inicio + fim + 1) // 2
 		if lista[meio] == (alvo):
-			print(qtdeComparacoes)
-			return (meio, qtdeComparacoes)
+			print(quant_comp)
+			return (meio, quant_comp)
 		elif lista[meio] < alvo:
 			inicio = meio + 1
 		else:
 			fim = meio - 1
-		return busca_binaria(lista[inicio:fim+1], alvo, qtdeComparacoes)
+		return busca_binaria(lista[inicio:fim+1], alvo, quant_comp)
 	else:
-		print(qtdeComparacoes)
-
+		print(quant_comp)
 
 lista = list(input().split(" "))
 alvo = lista.pop(0)
